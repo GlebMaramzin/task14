592e9e17c7e27a9e918e9ac3ee7ed57c144ddbd2
Gleb <g.maramzin@yandex.ru>
2021-02-02 11:58:53 +0300

    task 14

---
+++ Task_14_020221_TBD61/BOX.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_14_020221_TBD61
+{
+    //Описание класса BOX
+    class BOX
+    {
+        //Закрытые поля класса BOX
+        private int number;
+        private float width;
+        private float weight;
+        private float length;
+
+        //Конструктор без параметров, устанавливает значения полей класса по умолчанию
+        public BOX()
+        {
+            this.number = 20;
+            this.width = 10.2f;
+            this.weight = 2.4f;
+            this.length = 1.8f;
+        }
+
+        //Перегруженный конструктор с параметрами, через сеттеры устанавливает значения полей класса
+        public BOX(int number, float width, float weight, float length)
+        {
+            this.setNumber(number);
+            this.setWeight(width);
+            this.setWeight(weight);
+            this.setLength(length);
+        }
+
+        //Геттеры для полей класса
+        public int getNumber() => this.number;
+
+        public float getWidth() => this.width;
+
+        public float getWeight() => this.weight;
+
+        public float getLength() => this.length;
+
+        //Сеттеры для полей класса
+        public void setNumber(int number)
+        {
+            if (number > 0)
+            {
+                this.number = number;
+            }
+        }
+
+        public void setWidht(float width)
+        {
+            if (width > 0)
+            {
+                this.width = width;
+            }
+        }
+
+        public void setWeight(float weight)
+        {
+            if (weight > 0)
+            {
+                this.weight = weight;
+            }
+        }
+
+        public void setLength(float length)
+        {
+            if (length > 0)
+            {
+                this.length = length;
+            }
+        }
+    }
+}
---
+++ Task_14_020221_TBD61/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_14_020221_TBD61
+{
+    public partial class Form1 : Form
+    {
+        private List<BOX> boxes = new List<BOX>();
+        public Form1()
+        {
+            InitializeComponent();
+            
+            BOX box1 = new BOX();
+            BOX box2 = new BOX(5, 13.4f, 7.1f, 5.6f);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num = Convert.ToInt32(textBox1.Text);
+            float wdt = Convert.ToSingle(textBox2.Text);
+            float weig = Convert.ToSingle(textBox3.Text);
+            float len = Convert.ToSingle(textBox4.Text);
+
+            if (num > 0 || wdt > 0 || weig > 0 || len > 0 )
+            {
+                boxes.Add(new BOX(num, wdt, weig, len));
+
+                Console.WriteLine("Коробка добавлена в коллекцию");
+            }
+        }
+    }
+}
